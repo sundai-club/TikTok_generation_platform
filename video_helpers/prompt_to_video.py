13ec1dab161ceeaa1dc699b2ad5e1de572e20f1a
Cozy Shu <cozyshu@MacBook-Air.local>
2024-06-30 15:36:22 -0400

    add infinite zoom

--- video_helpers/prompt_to_video.py
+++ video_helpers/prompt_to_video.py
@@ -1,11 +1,27 @@
 import replicate
 
-input = {
-    "prompt": "A path going into the woods"
-}
+parsed_script = [
+  {
+    "text": "In a world where efficient travel and remote living were becoming increasingly important, a group of savvy globetrotters shared their secrets for streamlining life on the go.",
+    "prompt": "Discussing travel efficiency and remote living"
+  },
+  {
+    "text": "Nathalie introduced Earth Class Mail, a service that digitized physical mail, allowing nomads to manage their correspondence from anywhere.",
+    "prompt": "Introducing digital mail for nomads"
+  }
+]
+result = []
+for part in parsed_script:
 
-output = replicate.run(
-    "arielreplicate/stable_diffusion_infinite_zoom:a2527c5074fc0cf9fa6015a40d75d080d1ddf7082fabe142f1ccd882c18fce61",
-    input=input
-)
-print(output)
+    input = {
+        "prompt": part.get("prompt")
+    }
+
+    output = replicate.run(
+        "arielreplicate/stable_diffusion_infinite_zoom:a2527c5074fc0cf9fa6015a40d75d080d1ddf7082fabe142f1ccd882c18fce61",
+        input=input
+    )
+    part["mp4"] = output.get("mp4")
+    result.append(part)
+
+print(result)
