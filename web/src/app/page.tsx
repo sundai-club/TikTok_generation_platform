74a1370fd8f9f218f5069749f1af8f199ccd269b
Trevor Keith <tk@tkeith.com>
2024-06-30 16:42:03 -0400

    fix

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -18,7 +18,7 @@ function FileUploader() {
       {
         isDragActive ?
           <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag and drop some files here, or click to select files</p>
       }
       {files.length > 0 ? (
         <div className="mt-4">
